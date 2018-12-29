ef4f759e7178e253ee87bc4736182937cdda8bbb
Terance98 <thomasterance98@gmail.com>
2018-12-29 19:25:06 +0530

    This is the initial commit. The project is working perfectly fine till now

---
+++ index.js
@@ -0,0 +1,63 @@
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonClicked = this.innerHTML;
+    playAudio(buttonClicked);
+    buttonAnimation(buttonClicked);
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+  var keyPressed = event.key;
+  keyPressed = keyPressed.toLowerCase();
+  playAudio(keyPressed);
+  buttonAnimation(keyPressed);
+});
+
+function playAudio(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "a":
+      var kickBass = new Audio('sounds/kick-bass.mp3');
+      kickBass.play();
+      break;
+    case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeKey = document.querySelector("." + currentKey);
+  if (activeKey != null) {
+    activeKey.classList.add("pressed");
+
+    setTimeout(function() {
+      activeKey.classList.remove("pressed");
+    }, 100);
+  }
+}
